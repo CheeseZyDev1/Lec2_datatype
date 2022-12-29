81b4544765bc06c4b9f97f6a847cab6506e0668d
CheeseZyDev1 <s6406021622036@email.kmutnb.ac.th>
2022-12-29 15:17:10 +0700

    1

---
+++ onefuc.js
@@ -0,0 +1,11 @@
+// 1.Funtion Definition
+function printHello(name){
+    console.log("Hello "+name);
+    return name +"hello!";
+}
+
+console.log(printHello.name)
+
+// 2. Function Invocation
+let result = printHello("Napat !");
+console.log(result);
