2a2a2b8dc4cf33bae871b427f6824eaa6c7ddf56
CheeseZyDev1 <s6406021622036@email.kmutnb.ac.th>
2023-12-13 13:36:48 +0700

    1

--- ex02.js
+++ ex02.js
@@ -1,4 +1,4 @@
-function soEqua(equation) {
+/*unction soEqua(equation) {
    
     return eval(equation);
   }
@@ -6,23 +6,20 @@ function soEqua(equation) {
   console.log(soEqua("1 + 1"));  
   console.log(soEqua("7*4-2"));  
   console.log(soEqua("1+1+1+1+1"));  
-  
-  /*if not eval
+  */
   function solveEquation(equation) {
     // Split the equation into an array of tokens
     const tokens = equation.split(/\s+/);
   
     // Initialize a stack to hold the operands
     const stack = [];
-  
-    // Iterate through the tokens
+
     for (const token of tokens) {
-      // If the token is an operand, push it onto the stack
+
       if (!isNaN(token)) {
         stack.push(Number(token));
-      } else {
-        // If the token is an operator, pop the last two operands from the stack,
-        // perform the operation, and push the result back onto the stack
+      }
+       else {
         const operand2 = stack.pop();
         const operand1 = stack.pop();
         switch (token) {
@@ -50,4 +47,3 @@ function soEqua(equation) {
   console.log(solveEquation("2 + 2"));  // Output: 4
   console.log(solveEquation("3 * 5"));  // Output: 15
   console.log(solveEquation("(1 + 3) * 5"));  // Output: 20
-*/  
